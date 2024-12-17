aba4f45116a58dbb496a1347ae10999fa224b321
Rayerleier <1045643889@qq.com>
2024-12-17 21:52:29 +0800

    12.17

--- 9_ErrorHandling/_9_2_RecoverableErrorsWithResult/src/main.rs
+++ 9_ErrorHandling/_9_2_RecoverableErrorsWithResult/src/main.rs
@@ -1,21 +1,17 @@
 use std::fs::File;
+use std::io::{self, ErrorKind, Read};
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
 
-    let greeting_file = match greeting_file_result {
+    let greeting_file_1 = match greeting_file_result {
         Ok(file) => file,
         Err(error) => panic!("Problem opening the file: {error:?}"),
     };
-}
 
-use std::fs::File;
-use std::io::ErrorKind;
-
-fn main() {
-    let greeting_file_result = File::open("hello.txt");
-
-    let greeting_file = match greeting_file_result {
+    let greeting_file_result_2 = File::open("hello.txt");
+    // match on different errors
+    let greeting_file_2 = match greeting_file_result_2 {
         Ok(file) => file,
         Err(error) => match error.kind() {
             ErrorKind::NotFound => match File::create("hello.txt") {
@@ -27,13 +23,9 @@ fn main() {
             }
         },
     };
-}
 
-use std::fs::File;
-use std::io::ErrorKind;
-
-fn main() {
-    let greeting_file = File::open("hello.txt").unwrap_or_else(|error| {
+    // unwarp
+    let greeting_file_3 = File::open("hello.txt").unwrap_or_else(|error| {
         if error.kind() == ErrorKind::NotFound {
             File::create("hello.txt").unwrap_or_else(|error| {
                 panic!("Problem creating the file: {error:?}");
@@ -42,5 +34,59 @@ fn main() {
             panic!("Problem opening the file: {error:?}");
         }
     });
+
+    //  expect
+    let greeting_file_4 =
+        File::open("hello.txt").expect("hello.txt should be included in this project");
+        
 }
 
+/*
+   Propagating Errors
+   When a function's implementation calls something that might fail, instead of handling the error within the function itself
+   you can return the error to the calling code so that it can decide what to do.
+*/
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
+
+// a shortcut for propagating Errors: the ? operator
+fn read_username_from_file_1() -> Result<String, io::Error> {
+    // A function that returns errors to the calling code using the ? operator
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username);
+
+    // shorten more
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username);
+}
+
+/*
+    Where The ? Operator Can Be Used
+
+    The ? Operator can only be used in functions 
+    If we use ? Operator in Main, it will not compile
+
+    Luckily Main can also return a Result<(), E>
+
+    e.g.
+    fn main() -> Result<(), Box<dyn Error>> {
+    let greeting_file = File::open("hello.txt")?;
+
+    Ok(())
+}*/
---
+++ 9_ErrorHandling/_9_3_ToPanicOrNotPanic/src/main.rs
@@ -0,0 +1,13 @@
+/*
+    It's advisable to have your code panic when it's possible that your code could end up in a bad state.
+    In this context, a bad state is when some assumption,
+     guarantee, contract, or invariant has been broken, such as when invalid values, contradictory values, or missing values are passed to your code—plus one or more of the following:
+
+        * The bad state is something that is unexpected, as opposed to something that will likely happen occasionally, like a user entering data in the wrong format.
+        * Your code after this point needs to rely on not being in this bad state, rather than checking for the problem at every step.
+        * There's not a good way to encode this information in the types you use. We'll work through an example of what we mean in the "Encoding States and Behavior as Types" section of Chapter 17.
+
+ */
+
+
+
